6a4ff650f38a52c38350a198e0db632a551dad17
Kiloms <dos.kami@mail.ru>
2025-03-09 20:15:47 +0300

    Added material ui version3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    
+    // Добавляем поддержку загрузки шрифтов из Google Fonts
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -43,7 +47,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("io.socket:socket.io-client:2.0.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("io.socket:socket.io-client:2.0.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    
+    // Добавляем зависимость Google Fonts для загрузки шрифтов
+    implementation("androidx.core:core:1.12.0")
 }
